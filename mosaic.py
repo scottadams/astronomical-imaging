c34bb22e79712fb68ae6609fe45f90510a3979cc
Robert Hollinshead <rob_hollinshead@hotmail.co.uk>
2014-12-09 15:54:58 +0000

    just a few minor changes to mosaic.py

--- mosaic.py
+++ mosaic.py
@@ -2,6 +2,7 @@ import math							#import math
 from astropy.io import fits			#import fits
 import numpy as np					#import numpy
 import matplotlib.pyplot as plt		#import matplotlib
+import mask as ms 					#import mask
 
 
 mosaic = fits.open('mosaic.fits')	#loads fit file into mosaic
@@ -10,8 +11,19 @@ image = mosaic[0].data				#loads image with image data
 
 mag = hdr['MAGZPT']-2.5*np.log10(image)			#convert pixel count to magnitude
 
-image[30:40, 10:20] = 500						#set pixels y=31 to 40 and x=11 to 20 to 500
+mask = ms.perim(50, image)
+image[mask] = 0
+
+
+
 mosaic.writeto('newimage.fits', clobber=True)	#writes modified image to new fits file
 
 plt.imshow(image)					#renders image for matplotlib
 plt.show()							#draws image
+
+
+
+
+
+############ MIGHT BE USEFUL LATER ##############
+image[30:40, 10:20] = 500						#set pixels y=31 to 40 and x=11 to 20 to 500
