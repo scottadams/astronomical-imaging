adb814865605a43073c4d2e8e290798c64092a38
Scott Adams <s_j_adams@me.com>
2014-12-11 17:01:10 +0000

    made mask modules return image with mask applied. still need to add multimask functionality

--- mask.py
+++ mask.py
@@ -1,49 +1,46 @@
 import numpy as np
+import numpy.ma as ma
 
 
-def perim(borderdepth,image):
-	imgheight = image.shape[0]-1
-	imgwidth = image.shape[1]-1
-	perimask = np.ma.make_mask(image, copy=True, shrink=True,dtype=np.bool)
+def perim(image,borderdepth):
+# calculates and applies a boolean perimeter mask of given depth, returning the masked image
 
-	perimask[:,:] = False
+    imgheight = image.shape[0]-1
+    imgwidth = image.shape[1]-1
+    perimask = np.ma.make_mask(image, copy=True, shrink=True,dtype=np.bool)
 
-	for y in range(0,borderdepth):						
-		perimask[y,:]=True
-		perimask[imgheight-y,:]=True
-	
-	for x in range(0,borderdepth):
-		perimask[:,x]=True
-		perimask[:,imgwidth-x]=True
+    perimask[:,:] = False
 
-	return perimask
+    for y in range(0,borderdepth):
+        perimask[y,:]=True
+        perimask[imgheight-y,:]=True
+    
+    for x in range(0,borderdepth):
+        perimask[:,x]=True
+        perimask[:,imgwidth-x]=True
+
+    maskedimage = ma.array(image)                                #create maskable array using image data
+    maskedimage.mask = perimask                                  #set mask as mask
+
+    return maskedimage
 
 
-def sector(shape,centre,radius,angle_range):
-    """
-    Return a boolean mask for a circular sector. The start/stop angles in  
-    `angle_range` should be given in clockwise order.
-    """
+def circle(image,centre,radius):
+# calculates and applies a circular mask of given position and radius, returning the masked image
+
+    shape = image.shape
 
     x,y = np.ogrid[:shape[0],:shape[1]]
     cx,cy = centre
-    tmin,tmax = np.deg2rad(angle_range)
-
-    # ensure stop angle > start angle
-    if tmax < tmin:
-            tmax += 2*np.pi
 
     # convert cartesian --> polar coordinates
     r2 = (x-cx)*(x-cx) + (y-cy)*(y-cy)
-    theta = np.arctan2(x-cx,y-cy) - tmin
 
-    # wrap angles between 0 and 2*pi
-    theta %= (2*np.pi)
-
-    # circular mask
+    # generate circular mask
     circmask = r2 <= radius*radius
 
-    # angular mask
-    anglemask = theta <= (tmax-tmin)
+    maskedimage = ma.array(image)                                #create maskable array using image data
+    maskedimage.mask = circmask                        #set mask as mask
+    
 
-    return circmask*anglemask
+    return maskedimage
