58bb92f0929e1bac83fab95c1bb08eff51059727
Robert Hollinshead <rob_hollinshead@hotmail.co.uk>
2014-12-29 14:00:36 +0000

    got rid of random bits that were added to background.py

--- background.py
+++ background.py
@@ -1,15 +1,11 @@
-<<<<<<< HEAD
 from astropy.io import fits                 #import fits
-=======
 from astropy.io import fits #import fits
->>>>>>> fff8cd7cfb40e7866d56fa7946173ecb52be2465
 import numpy as np
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 import numpy as np
 
 def calc(image):
-<<<<<<< HEAD
     shape = image.shape
     freqarray = []
     #frequencies = open('frequencies.txt','a')
@@ -25,7 +21,6 @@ def calc(image):
 mosaic = fits.open('mosaic.fits')   #loads fit file into mosaic
 hdr = mosaic[0].header              
 image = mosaic[0].data              
-=======
 	shape = image.shape
 	freqarray = []
 	#frequencies = open('frequencies.txt','a')
@@ -41,16 +36,12 @@ image = mosaic[0].data
 mosaic = fits.open('mosaic.fits') #loads fit file into mosaic
 hdr = mosaic[0].header
 image = mosaic[0].data
->>>>>>> fff8cd7cfb40e7866d56fa7946173ecb52be2465
 
 x = calc(image)
 
 # the histogram of the data
-<<<<<<< HEAD
 n, bins, patches = plt.hist(x, 1500, normed=1, facecolor='green', alpha=0.75)
-=======
 n, bins, patches = plt.hist(x, 2000, normed=1, facecolor='green', alpha=0.75)
->>>>>>> fff8cd7cfb40e7866d56fa7946173ecb52be2465
 
 # add a 'best fit' line
 # y = mlab.normpdf( bins, mu, sigma)
@@ -59,13 +50,9 @@ n, bins, patches = plt.hist(x, 2000, normed=1, facecolor='green', alpha=0.75)
 plt.xlabel('frequency')
 plt.ylabel('counts')
 plt.title(r'histogram')
-<<<<<<< HEAD
 plt.axis([3300, 3600, 0, 0.06])
 plt.grid(True)
 plt.show()
-
-=======
 plt.axis([3300, 3600, 0, 0.1])
 plt.grid(True)
 plt.show()
->>>>>>> fff8cd7cfb40e7866d56fa7946173ecb52be2465
